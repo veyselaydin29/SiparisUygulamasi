d0e9e9eb81fa25b5b8b2c151a6d176db6dad7095
veyselaydin29 <veyselaydin29@gmail.com>
2024-04-07 02:14:45 +0300

    Building first UI - Just following tutorial.

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,34 +1,43 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/src/components/EditScreenInfo';
-import { Text, View } from '@/src/components/Themed';
+import { StyleSheet, Image, Text, View } from 'react-native';
 
 import products from '../../../assets/data/products';
 const product = products[0];
 
+import Colors from '@/src/constants/Colors';
+
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="src/app/(tabs)/index.tsx" />
+      <Image
+        source={{ uri: product.image }}
+        style={styles.image}
+        resizeMode="contain"></Image>
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>{product.price.toFixed(2)} TL</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 10,
+    overflow: 'hidden',
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  image:{
+    width: '100%',
+    aspectRatio: 1,
+    alignSelf: 'center',
   },
+  price:{
+    color: Colors.light.tint,
+    fontWeight: 'bold',
+    marginTop: 'auto',
+  },
+
 });
